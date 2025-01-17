17e243566eb23b94db697079531c4dece4998473
Crystal <sulemenov9@gmail.com>
2025-01-17 12:28:06 +0600

    Good

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter/rendering.dart';
+
 void main() {
-  //debugPaintSizeEnabled = true;
+
   runApp(const MyApp());
 }
 
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: primaryColor,
         colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
-        useMaterial3: false,
+        useMaterial3: true,
       ),
       home: const HomeScreen(),
     );
@@ -32,28 +32,18 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  String _querySearch = "";// переменная состояния запроса
-  late  List<Person> _filteredPersons;// переменная для отфильтрофанных значений, late означает что переменная будет объявлена позже
+  String querySearch = ""; // переменная состояния запроса
+  late List<Person> _filteredPersons; // переменная для отфильтрофанных значений // late означает что переменная будет определена позже
   int _selectedIndex = 0;
 
   // Список персонажей
   final List<Person> persons = [
-    Person('Рик Санчез', 'Человек, Мужской', true, 'assets/images/Rick.jpg'),
-    Person('Рик Санчез', 'Человек, Мужской', true, 'assets/images/Rick.jpg'),
-    Person('Рик Санчез', 'Человек, Мужской', true, 'assets/images/Rick.jpg'),
-    Person('Директор Агенства', 'Человек, Мужской', true, 'assets/images/Director.jpg'),
-    Person('Морти Смит', 'Человек, Мужской', true, 'assets/images/Morty.jpg'),
-    Person('Саммер Смит', 'Человек, Женский', true, 'assets/images/Sammer.jpg'),
-    Person('Рик Санчез', 'Человек, Мужской', true, 'assets/images/Rick.jpg'),
-    Person('Рик Санчез', 'Человек, Мужской', true, 'assets/images/Rick.jpg'),
     Person('Рик Санчез', 'Человек, Мужской', true, 'assets/images/Rick.jpg'),
     Person('Директор Агенства', 'Человек, Мужской', true, 'assets/images/Director.jpg'),
     Person('Морти Смит', 'Человек, Мужской', true, 'assets/images/Morty.jpg'),
     Person('Саммер Смит', 'Человек, Женский', true, 'assets/images/Sammer.jpg'),
     Person('Альберт Эйнштейн', 'Человек, Мужской', false, 'assets/images/Albert.jpg'),
     Person('Алан Райлс', 'Человек, Мужской', false, 'assets/images/Alan.jpg'),
-    Person('Рик Санчез', 'Человек, Мужской', true, 'assets/images/Rick.jpg'),
-    Person('Рик Санчез', 'Человек, Мужской', true, 'assets/images/Rick.jpg'),
     Person('Рик Санчез', 'Человек, Мужской', true, 'assets/images/Rick.jpg'),
     Person('Директор Агенства', 'Человек, Мужской', true, 'assets/images/Director.jpg'),
     Person('Морти Смит', 'Человек, Мужской', true, 'assets/images/Morty.jpg'),
@@ -68,19 +58,23 @@ class _HomeScreenState extends State<HomeScreen> {
       _selectedIndex = index;
     });
   }
-  void initState(){
+
+  @override
+  void initState() {
     super.initState();
-    _filteredPersons = persons; // назначаем что при пустой строке поиска будет показываться весь список персов
+    _filteredPersons = persons; // initState используется для инициализации нашей переменой чтоб изначально показывался весь список
   }
-  void _filterPersons(String query){
+
+  void _filterPersons(String query) {
     setState(() {
-      _querySearch = query;
-      if (query.isEmpty){
-       _filteredPersons = persons;// булевое значение, если строка пуста то возвращается true или же весь наш список
-      } else{
-        _filteredPersons = persons.where((Person){//.where в Dart — это метод для фильтрации элементов в коллекциях, таких как списки. Он принимает функцию-предикат в качестве аргумента и возвращает новый итерабельный объект, содержащий только элементы, которые удовлетворяют условию.
-          return Person.name.toLowerCase().contains(query.toLowerCase());// contains сверяет данные в именах персонажей и в запросе
-        }).toList();
+      querySearch = query;
+      if (query.isEmpty) { // булевое значение, если строка пуста то возвращается true или же весь наш список
+
+        _filteredPersons = persons;
+      } else {
+        _filteredPersons = persons.where((person) {
+          return person.name.toLowerCase().contains(query.toLowerCase());// это метод для фильтрации элементов в коллекциях, таких как списки. Он принимает функцию-предикат в качестве аргумента и возвращает новый итерабельный объект, содержащий только элементы, которые удовлетворяют условию.
+        }).toList();// contains сверяет данные в именах персонажей и в запросе
       }
     });
   }
@@ -89,77 +83,74 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Scaffold(
-      body:
-      CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            bottom: PreferredSize(
-              preferredSize:  Size.fromHeight(48),
-              child: SearchBotton(theme: theme, onSearch: _filterPersons,),
-            ),
-
-            pinned: true,
-            snap: true,
-            floating: true,
-            backgroundColor: theme.primaryColor,
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-              margin:  EdgeInsets.only(left: 16, right: 15,  bottom: 20,top: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding:  EdgeInsets.only(top: 4, bottom: 4),
-                    child: Text(
-                      'Количество персонажей: ${persons.length}', // Отображаем количество персонажей
-                      style: TextStyle(
+      appBar: AppBar(
+        titleSpacing: 0,
+        automaticallyImplyLeading: false,// убрал отступы по умолчанию
+        backgroundColor: const Color(0xfff0B1E2D),
+        title: SearchBotton(theme: theme, onSearch: _filterPersons),
+        
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(left: 16, right: 15, bottom: 20,),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 4, bottom: 4),
+                  child: Text(
+                    'ВСЕГО ПЕРСОНАЖЕЙ: ${persons.length}',
+                    style: const TextStyle(
                         fontWeight: FontWeight.w500,
-                        color: Colors.grey,
+                        color: Color(0xfff5B6975),
                         fontSize: 10,
-                      ),
+                        letterSpacing: 1.5
                     ),
                   ),
-                  Padding(
-                    padding:  EdgeInsets.only(right: 14),
-                    child: Icon(Icons.grid_view_outlined, color: Colors.grey),
-                  ),
-                ],
-              ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(right: 14),
+                  child: Icon(Icons.grid_view_outlined, color: Color(0xfff5B6975)),
+                ),
+              ],
             ),
           ),
-          PersonsList(persons: persons),
+          Expanded(
+            child: PersonsList(persons: _filteredPersons),
+          ),
         ],
       ),
-
-      bottomNavigationBar:BottomNavigationBar(
+      bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
         selectedItemColor: Colors.green,
         unselectedItemColor: Colors.grey,
         backgroundColor: const Color(0xfff152a3a),
-        items:   [
-          BottomNavigationBarItem(backgroundColor:theme.primaryColor ,
-            icon: ImageIcon(AssetImage('assets/images/guest.png')),
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: theme.primaryColor,
+            icon: const ImageIcon(AssetImage('assets/images/guest.png')),
             label: 'Персонажи',
           ),
-          BottomNavigationBarItem(backgroundColor:theme.primaryColor ,
-            icon: ImageIcon(AssetImage('assets/images/location.png')),
+          BottomNavigationBarItem(
+            backgroundColor: theme.primaryColor,
+            icon: const ImageIcon(AssetImage('assets/images/location.png')),
             label: 'Локации',
           ),
-          BottomNavigationBarItem(backgroundColor:theme.primaryColor ,
-            icon: ImageIcon(AssetImage('assets/images/episode.png')),
+          BottomNavigationBarItem(
+            backgroundColor: theme.primaryColor,
+            icon: const ImageIcon(AssetImage('assets/images/episode.png')),
             label: 'Эпизоды',
           ),
-          BottomNavigationBarItem(backgroundColor:theme.primaryColor ,
-            icon: ImageIcon(AssetImage('assets/images/Settings.png')),
+          BottomNavigationBarItem(
+            backgroundColor: theme.primaryColor,
+            icon: const ImageIcon(AssetImage('assets/images/Settings.png')),
             label: 'Настройки',
           ),
         ],
       ),
-
-
-      backgroundColor: theme.primaryColor,
+      backgroundColor: const Color(0xfff0B1E2D),
     );
   }
 }
@@ -170,59 +161,49 @@ class SearchBotton extends StatelessWidget {
     required this.theme,
     required this.onSearch,
   });
+
   final ThemeData theme;
   final Function(String) onSearch;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-
-      margin: const EdgeInsets.symmetric(horizontal: 10),
+      margin: const EdgeInsets.only(left: 16,right: 16,top: 11,bottom:  20,),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(16),
-        color: theme.highlightColor,
+        color: Color(0xfff152A3A) ,
       ),
-      child:  Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Row(
         children: [
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only( left: 15, top: 12, bottom: 12),
-                child: const Icon(Icons.search_rounded),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10),
+            child: const Icon(Icons.search_rounded,color: Color(0xfff5B6975),),
+          ),
+          Expanded(
+            child: TextField(
+              onChanged: onSearch,
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Найти персонажа',
               ),
-
-              Container(
-
-                margin: const EdgeInsets.only(left: 10,right: 59),
-                padding: const EdgeInsets.only( top: 12, bottom: 12),
-
-                child: Text(
-                  'Найти персонажа',
-                 style: TextStyle(
-                    fontWeight: FontWeight.w400,
-                    color: theme.hintColor,
-                    fontSize: 16,
-                  ),
-                    //border: InputBorder.none,
-                  ),
-                ),
-
-            ],
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                color: Color(0xfff5B6975),
+                fontSize: 16,
+              ),
+            ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 10),
-            child: const Text('|'),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 15),
-            child: const Icon(Icons.filter_alt_outlined),
+          Container(child: Text('|',style: TextStyle(color: Color(0xfff5B6975)),),),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10),
+            child: const Icon(Icons.filter_alt_outlined,color: Color(0xfff5B6975),),
           ),
         ],
       ),
     );
   }
 }
+
 
 class Person {
   final String name;
@@ -239,54 +220,50 @@ class PersonsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    return SliverList(
-      delegate: SliverChildBuilderDelegate(
-            (BuildContext context, int index) {
-          final person = persons[index];
-
-          return Container(
-            decoration: BoxDecoration(color: theme.primaryColor),
-            margin: const EdgeInsets.symmetric(horizontal: 16).copyWith(bottom: 24),
-            child: ListTile(
-              contentPadding: EdgeInsets.zero,
-              leading: CircleAvatar(
-                radius: 40,
-                backgroundImage: AssetImage(person.imagePath),
-              ),
-              title: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    person.state ? 'Живой' : 'Мертвый',
-                    style: TextStyle(
-                      color: person.state ? Colors.green : Colors.red,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 10,
-                    ),
-                  ),
-                  Text(
-                    person.name,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  ),
-                  Text(
-                    person.status,
-                    style: TextStyle(
-                      color: Colors.grey[400],
-                      fontSize: 12,
-                    ),
-                  ),
-                ],
-              ),
+    return ListView.builder(
+      itemCount: persons.length,
+      itemBuilder: (context, index) {
+        final person = persons[index];
+        return Container(
+          decoration: const BoxDecoration(color: Color(0xfff0B1E2D)),
+          margin: const EdgeInsets.symmetric(horizontal: 16).copyWith(bottom: 24),
+          child: ListTile(
+            contentPadding: EdgeInsets.zero,
+            leading: CircleAvatar(
+              radius: 40,
+              backgroundImage: AssetImage(person.imagePath),
             ),
-          );
-        },
-        childCount: persons.length,
-      ),
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  person.state ? 'Живой' : 'Мертвый',
+                  style: TextStyle(
+                    color: person.state ? Colors.green : Colors.red,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 10,
+                  ),
+                ),
+                Text(
+                  person.name,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                Text(
+                  person.status,
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
